cffcd0b97462aebb62a252d4d8b470c6f8ac27b3
asim khalil <aximmak11@gmail.com>
2023-04-17 01:39:49 +0500

    task3

---
+++ task3.py
@@ -0,0 +1,22 @@
+from utils import scroll_to_page_end, extract_videos_data
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+page_url = input('Please enter your YouTube URL :')
+
+driver = webdriver.Chrome()
+driver.get(page_url)
+
+scroll_to_page_end(driver)
+
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+
+video_data = extract_videos_data(soup)
+
+data_frame = pd.DataFrame(video_data)
+data_frame.to_csv(f"{page_url.split('/')[-2]}.csv", index=False)
+
+print('Extracted data save to CSV file')
