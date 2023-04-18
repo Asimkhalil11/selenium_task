e03d0ba61beccca7a701661c57be82237060575e
asim khalil <aximmak11@gmail.com>
2023-04-19 02:21:00 +0500

    CSV file was empty, Now with data

--- task3.py
+++ task3.py
@@ -4,16 +4,16 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 
-page_url = input('Please enter your YouTube URL :')
+page_url = input('Please enter your YouTube URL: ')
 
 driver = webdriver.Chrome()
+# driver = webdriver.Chrome(executable_path="chromedriver")
 driver.get(page_url)
 
 scroll_to_page_end(driver)
 
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
-
 video_data = extract_videos_data(soup)
 
 data_frame = pd.DataFrame(video_data)
